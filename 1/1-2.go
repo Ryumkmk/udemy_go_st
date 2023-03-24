6b39a19dea4202b508ac3b701f064006d6c9c41a
Ryumkmk <luihao1999929@icloud.com>
2023-03-24 11:20:39 +0900

    1-2

--- 1/1-2.go
+++ 1/1-2.go
@@ -1,14 +1,23 @@
 package main
 
+import "fmt"
+
 type User struct {
 	Name string
 	Age  int
 }
 
 func (u User) SayName() {
-	fmt.println(u.Name)
+	fmt.Println(u.Name)
+}
+
+func (u *User) SetName(name string) {
+	u.Name = name
 }
 func main() {
 	user1 := User{Name: "Ryu"}
 	user1.SayName()
+	user1.SetName("Ko")
+	user1.SayName()
+
 }
